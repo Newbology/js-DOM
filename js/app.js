874e72a8f3e96da9e0a2d4d47c7fa38d18c3d615
kevin tauzer <kevintauzer97@gmail.com>
2018-10-19 12:53:54 -1000

    finished work

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@
 Find the div with the id of "name1" and replace the n/a with the following:
 
 Tay-Tay*/
-
+var taytay = document.getElementById("name1");
+taytay.innerHTML = "Tay-Tay";
 
 /*2. DJ Khaled
 
@@ -14,12 +15,16 @@ Find the div with the id of "position2" and replace the n/a with the following:
 Project Manager*/
 
 
+var djPos = document.getElementById("position2");
+djPos.innerHTML = "Project Manager";
+
 /*3. Piko Taro
 
 Find the div with the id of "alias3" and replace the n/a with the following:
 
 Concatenation*/
-
+var pikoAlias = document.getElementById("alias3");
+pikoAlias.innerHTML = "Concatenation";
 
 
 
@@ -27,19 +32,23 @@ Concatenation*/
 
 Find the div with the class name of "profile" and replace the n/a with a verse from your favorite Prince song.*/
 
+var profElem = document.getElementsByClassName("profile");
+profElem[0].innerHTML = "Purple rain, purple rain";
 
 
 /*5. Bruce Lee
 
 Find the div with the class name of "profile" and replace the n/a with a quote from the legend himself.*/
 
-
+profElem[1].innerHTML = "“A goal is not always meant to be reached, it often serves simply as something to aim at.” "
 
 /*6. Samuel L Jackson
 
 Find the div with the class name of "alias" and replace the n/a with your favorite character that Sammy portrayed.*/
 
-
+var aliasElem = document.getElementsByClassName("alias");
+aliasElem[2].innerHTML = "Frozone";
+profElem[2].innerHTML = "HONEY....WHERES MY SUPER SUIT?????";
 
 /*7. Peter Griffin
 
@@ -47,7 +56,10 @@ Create a div element and give it an id of "name7". Inside this div element, give
 
 Append this div element to the nameParent div*/
 
-
+var peterName = document.createElement("div");
+peterName.id = "name7";
+peterName.innerHTML = "Peter Griffin";
+document.getElementById("nameParent").appendChild(peterName);
  
 /*8. Tim Duncan
 
@@ -55,7 +67,10 @@ Create a div element give it an id of "alias8". Inside this div element, give it
 
 Append this div element to the aliasParent div.*/
 
-
+var alias8 = document.createElement("div");
+alias8.id = " alias8";
+alias8.innerHTML = "Old Man Riverwalk";
+document.getElementById("aliasParent").appendChild(alias8);
 
 //Final Boss
 /*9. Create your own profile.*/
